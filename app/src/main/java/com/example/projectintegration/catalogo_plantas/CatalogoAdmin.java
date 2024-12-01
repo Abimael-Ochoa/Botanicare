6b4427da337a415780b7052ec8b7129b16d53d16
Alexis_Reyes <202160062@ucc.mx>
2024-11-30 18:22:15 -0600

    update

--- app/src/main/java/com/example/projectintegration/catalogo_plantas/CatalogoAdmin.java
+++ app/src/main/java/com/example/projectintegration/catalogo_plantas/CatalogoAdmin.java
@@ -1,20 +1,28 @@
-package com.example.projectintegration;
+package com.example.projectintegration.catalogo_plantas;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.projectintegration.R;
 
 public class CatalogoAdmin extends AppCompatActivity {
+
+    private PantallaCatalogo pantallaCatalogo;
+
+    private CatalogoAdmin(PantallaCatalogo pantallaCatalogo) {
+        this.pantallaCatalogo = pantallaCatalogo;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catalogo_admin);
+
+
 
     }
 }
