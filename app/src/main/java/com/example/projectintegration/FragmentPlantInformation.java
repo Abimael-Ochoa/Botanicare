abe2883b74918e1ac98c49b0580f88ee850e79d6
leonardoucc <202160423@ucc.mx>
2024-12-01 01:58:11 -0600

    terminado lo de mis plantas v2

--- app/src/main/java/com/example/projectintegration/FragmentPlantInformation.java
+++ app/src/main/java/com/example/projectintegration/FragmentPlantInformation.java
@@ -59,6 +59,6 @@ public class FragmentPlantInformation extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plant_information, container, false);
+        return inflater.inflate(R.layout.fragment_plant_information_user, container, false);
     }
 }
